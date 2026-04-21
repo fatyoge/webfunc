1789f30eb24d94c286121936515a8a751e6058b5
ouruibin <ouruibin@gf.com.cn>
2026-04-21 14:31:50 +0800

    feat: export SkillLoader and SkillModule types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { BrowserBridge, BrowserBridgeOptions } from './core/browser-bridge';
 export { CookieStore } from './core/cookie-store';
 export { SkillExecutor } from './core/executor';
 export { SkillRecorder } from './core/recorder';
+export { SkillLoader, type LoadedSkill } from './core/skill-loader';
 export { renderTemplate, renderSkillStep } from './core/template-renderer';
 export { LLMParser, LLMParserOptions } from './llm/parser';
 export * from './types/skill';
+export * from './types/skill-module';
